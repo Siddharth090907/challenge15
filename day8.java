784ba342db6e9be98b019f06ee2184757ca8c861
Siddharth090907 <164858666+Siddharth090907@users.noreply.github.com>
2025-08-22 22:08:17 +0530

    Create day8.java
    Day 8 complete

---
+++ day8.java
@@ -0,0 +1,20 @@
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        // Trim leading/trailing spaces and split by one or more spaces
+        String[] words = s.trim().split("\\s+");
+
+        // Reverse the array of words
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i != 0) reversed.append(" ");
+        }
+
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "   Java   is   awesome   ";
+        System.out.println(reverseWords(s)); // Output: "awesome is Java"
+    }
+}
